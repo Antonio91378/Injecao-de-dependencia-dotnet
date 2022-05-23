c4a3bc9160942967acb2676e8e4c830f999f0920
Antonio <antoniovictor200@gmail.com>
2022-05-23 13:12:04 -0300

    primeiro commit

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly IOperacao _operacao;
+    private readonly IServiceProvider _provider;
+    public WeatherForecastController(IOperacao operacao, IServiceProvider provider){
+        _operacao = operacao;
+        _provider = provider;
+    }
+    [HttpGet("Construtor")]
+    public IActionResult Construtor(){
+        return Ok(_operacao.Id);
+    }
+    [HttpGet("Anotacao")]
+    public IActionResult Anotacao([FromServices] IOperacao operacao ){
+        return Ok(operacao.Id);
+    }
+    [HttpGet("Provider")]
+    public IActionResult Provider(){
+        return Ok(_provider.GetRequiredService<IOperacao>());
+    }
+}
---
+++ API/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace API;
+public class Operacao : IOperacao
+{
+    public Guid Id { get; set; }
+    public Operacao(){
+        Id = Guid.NewGuid();
+    }
+}
+public interface IOperacao
+{
+    Guid Id { get; set; }
+}
